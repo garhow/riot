9ccbbb0481a175de877d85b0c33ad8ad559f5ba8
Garrett Howard <garrett@mersh.com>
2021-11-14 03:20:59 -0600

    Added tweening for network peer bodies

--- src/scenes/controllers/peer.gd
+++ src/scenes/controllers/peer.gd
@@ -4,7 +4,8 @@ class_name Peer
 var velocity : Vector3
 
 func update(t : Vector3, ry : float, hrx : float, vel : Vector3):
-	translation = t
-	transform.basis = Basis(Vector3(rotation.x, ry, rotation.z))
+	$Tween.interpolate_property(self, "translation", translation, t, Net.HOST_RATE, Tween.TRANS_LINEAR)
+	$Tween.interpolate_property(self, "transform:basis", transform.basis, Basis(Vector3(rotation.x, ry, rotation.z)), Net.HOST_RATE, Tween.TRANS_LINEAR)
+	$Tween.start()
 	get_node("Head").rotation.x = hrx
 	velocity = vel
